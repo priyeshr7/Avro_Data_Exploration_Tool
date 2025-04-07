c2df4b3229d6f35d3e5ac8c11155a73a804a0a1d
priyesh <priyeshr067@gmail.com>
2025-04-07 09:51:26 +0530

    package name fixing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Avro_Data_Exploration_Tool",
+    name="avro_data_exploration_tool",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
